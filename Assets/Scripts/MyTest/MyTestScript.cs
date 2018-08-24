04f9bb56c76a4623e9139e7d479de83f2b8aa07d
Peter Karlsson <peter.karlsson90@icloud.com>
2018-08-24 10:03:13 +0200

    TestScene with isTouching

---
+++ Assets/Scripts/MyTest/MyTestScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTestScript : MonoBehaviour {
+
+    public GameObject redSquare;
+
+    Collider2D greenCollider;
+    Collider2D redCollider;
+
+    SpriteRenderer spriteRenderer;
+    Color startColor;
+
+    private void Start()
+    {
+        greenCollider = GetComponent<Collider2D>();
+        redCollider = redSquare.GetComponent<Collider2D>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        startColor = spriteRenderer.color;
+
+        //greenCollider.isTrigger = true;
+        //redCollider.isTrigger = true;
+    }
+
+    private void Update() {
+        if (redCollider.IsTouching(greenCollider)) {
+            spriteRenderer.color = Color.yellow;   
+        } else {
+            spriteRenderer.color = startColor;
+        }
+    }
+}
